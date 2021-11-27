4bb0d085c3beeb1c017b4acfb01f5315b11a3295
iasd22_0887 <iasd22_0887@readycash.nat.lamsade.dauphine.fr>
2021-11-27 12:26:20 +0100

    MobileNetv2 Models

---
+++ restart.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+
+from tensorflow.keras import layers 
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, GlobalAveragePooling2D, Reshape
+from MobileNetBaseFile import MobileNetBase 
+
+
+import golois
+
+planes = 31
+moves = 361
+N = 10000
+epochs = 400
+batch = 128
+filters = 64
+
+Exec = True
+
+if Exec:
+    input_data = np.random.randint(2, size=(N, 19, 19, planes))
+    input_data = input_data.astype ('float32')
+
+    policy = np.random.randint(moves, size=(N,))
+    policy = keras.utils.to_categorical (policy)
+
+    value = np.random.randint(2, size=(N,))
+    value = value.astype ('float32')
+
+    end = np.random.randint(2, size=(N, 19, 19, 2))
+    end = end.astype ('float32')
+
+    groups = np.zeros((N, 19, 19, 1))
+    groups = groups.astype ('float32')
+
+    print ("getValidation", flush = True)
+    golois.getValidation (input_data, policy, value, end)
+
+
+
+model = keras.models.load_model('mb3_1M_200_0005.h5.h5')
+
+model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.0005, momentum=0.9),
+              loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
+              loss_weights={'policy' : 1.0, 'value' : 1.0},
+              metrics={'policy': 'categorical_accuracy', 'value': 'mse'})
+
+for i in range (1, epochs + 1):
+    print ('epoch ' + str (i))
+    golois.getBatch (input_data, policy, value, end, groups, i * N)
+    history = model.fit(input_data,
+                        {'policy': policy, 'value': value}, 
+                        epochs=1, batch_size=batch,verbose=0)
+    if (i % 10 == 0):
+        golois.getValidation (input_data, policy, value, end)
+        val = model.evaluate (input_data,
+                              [policy, value], verbose = 0, batch_size=batch)
+        print ("val =", val)
+
+model.save("mb3_1M_600_00005.h5")
